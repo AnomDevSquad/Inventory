a461347d7a198bf7c4893b97e7b83695ae5cd05c
xentyo <xentyo@hotmail.com>
2016-10-24 14:07:54 -0700

    asd
    asd

---
+++ forms.php
@@ -0,0 +1,7 @@
+<?php $title = 'Inventory form'; include('/../templates/includes/_header.html'); ?>
+<section class="container">
+  <form action="">
+    
+  </form>
+</section>
+<?php include('/../templates/includes/_footer.html'); ?>
