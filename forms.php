22fec2f634ac4ebd025b0457b3a881c4b28c1466
xentyo <xentyo@hotmail.com>
2016-11-06 08:59:58 -0800

    Main menu
    added a primary interface. When you click it anyone you need to sign as
    somebody that can to see the content

--- forms.php
+++ forms.php
@@ -1,7 +1,7 @@
-<?php $title = 'Inventory form'; include('/../templates/includes/_header.html'); ?>
+<?php $title = 'Inventory form'; include('/templates/includes/_header.html'); ?>
 <section class="container">
   <form action="">
-    
+
   </form>
 </section>
-<?php include('/../templates/includes/_footer.html'); ?>
+<?php include('/templates/includes/_footer.html'); ?>
--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 function init(){
-  var cardbox = document.getElementById('cardbox');
-  var card = new Card(cardbox, 'div','Inventory', 'Icon', '','card');
-  card.draw();
+  document.getElementById('header').innerHTML = sessionStorage['where'];
+}
+
+function initlogin(){
+  document.title = sessionStorage['where'];
+}
+
+function setsite(where){
+  sessionStorage['where'] = where;
+  window.location = 'login.html';
+}
+
+function login(){
+  window.location = loginsite();
+}
+
+function loginsite(){
+  var pages = [['Inventory','inventory.html'], ['Dashboard','dashboard.html'], ['Orders','orders.html']];
+  for(var i = 0; i<pages.length; i++){
+    if(pages[i][0] == sessionStorage['where']){
+      return pages[i][1];
+    }
+  }
+  return '';
 }
