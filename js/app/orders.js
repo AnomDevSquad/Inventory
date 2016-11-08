4a7038ab165fb51cc314373ddf0ae5c7a581a691
CesarRamos <cesarito052@gmail.com>
2016-11-08 09:26:56 -0800

    haciendo la api de orders.js y una pre-view

--- js/app/orders.js
+++ js/app/orders.js
@@ -1,9 +1,11 @@
+var total = 0;
+
 function init(){
   loadDishes();
 }
 
 function loadDishes(){
-  var table = document.getElementById('table');
+  var table = document.getElementById('dishes');
   var request = new XMLHttpRequest();
   request.open('GET', 'http://localhost:8080/4to/Inventory/api/v1/get_all_dishes.php', true);
   request.send();
@@ -15,14 +17,38 @@ function loadDishes(){
         var tr = document.createElement('tr');
         var name = document.createElement('td');
         var price = document.createElement('td');
+        var button = document.createElement('button');
 
-        name.innerHTML = dish.name;
+        price.setAttribute('id', dish.id);
+
+        button.setAttribute('id', dish.id);
+        button.setAttribute('price', dish.price);
+        button.setAttribute('class', 'btn');
+        button.setAttribute('onclick', 'get_dish(this.id)');
+
+        button.innerHTML = dish.name;
         price.innerHTML = dish.price;
 
+        name.appendChild(button);
         tr.appendChild(name);
         tr.appendChild(price);
         table.appendChild(tr);
       }
     }
+  }
+}
+
+function get_dish(id){
+  var price = document.getElementById(id).getAttribute('price');
+  var t = document.getElementById('total');
+  total += parseInt(price);
+  t.innerHTML = total;
+}
+
+function buy(){
+  if (total > 0) {
+    alert("Total: " + total);
+  } else {
+    alert("You not selected anything Order");
   }
 }
