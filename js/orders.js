52b1942b001e349a355bff270e4067fa80c90d8d
CesarRamos <cesarito052@gmail.com>
2016-11-28 12:58:05 -0800

    modifique el js orders para para realizar una nueva orden, agregando un form de inputs del tipo hidden en el cual se embian los id's de cada platillo :D

--- js/orders.js
+++ js/orders.js
@@ -1,8 +1,8 @@
-var idCateory = [1,2,3,4,5];
+var idCategory = [1,2,3,4,5];
 var categoryName = ['Entrace','Sushi and Sashimi','Main Course','Tempura','Drinks'];
 var total = 0;
 var iva = 0;
-var arrayItems = [];
+var itemsArray = [];
 
 function orderInit(){
   setTimeout(dishesRequest, 1000);
@@ -16,7 +16,7 @@ function dishesRequest(){
   request.send();
   request.onreadystatechange = function(){
     if (request.status == 200 && request.readyState == 4) {
-      loadSections(content)
+      loadSections(content);
       loadDishes(request.responseText);
     }
   }
@@ -25,7 +25,7 @@ function dishesRequest(){
 function loadSections(content){
   var menu = create(content, 'div', ['id', 'class'], ['menu', ''])
   for (var i = 0; i < categoryName.length; i++) {
-    create(menu, 'div', ['id', 'class'], ["category_"+idCateory[i], 'category'], '<p>'+categoryName[i]+'</p>');
+    create(menu, 'div', ['id', 'class'], ["category_"+idCategory[i], 'category'], '<p>'+categoryName[i]+'</p>');
   }
 }
 
@@ -35,11 +35,13 @@ function loadDishes(data){
   for (var i = 0; i < dishes.length; i++) {
     var item = dishes[i];
     var section = document.getElementById('category_'+item.category);
-    create(section, 'button', ['id', 'value', 'onclick'], ['dish_'+item.id, 'button', 'addDish(this.id)'], item.name + ", $ " + item.price + " DLLS");
+    create(section, 'button', ['id', 'onclick', 'price'], ['dish_'+item.id, 'addDish(this.id)', item.price], item.name + ", $ " + item.price + " DLLS");
   }
   var content = document.querySelector('#main_content');
   var order = create(content, 'div', ['id', 'class'], ['orders', ''], 'Orders');
   create(order, 'div', ['id'], ['dishes']);
+  var form = create(order, 'form', ['id', 'method'], ['form', 'post']);
+  create(form, 'input', ['id', 'class', 'onclick', 'type', 'value'], ['submit', 'btn', 'buy();', 'button', 'Buy']);
 }
 
 function addDish(id){
@@ -54,4 +56,24 @@ function removeDish(id){
   var item = document.getElementById(id);
   alert(item.innerHTML);
   item.parentNode.removeChild(item);
+}
+
+function buy(){
+  var dishes = document.getElementById('dishes').childNodes;
+  for (var i = 0; i < dishes.length; i++) {
+    itemsArray.push(dishes[i].getAttribute('id'));
+  }
+  var form = document.getElementById('form');
+  for (var i = 0; i < itemsArray.length; i++) {
+    create(form, 'input', ['id', 'type', 'name'], [itemsArray[i] , 'hidden', 'dishes[]']);
+  }
+  // var request = new XMLHttpRequest();
+  // request.open('POST', url, true);
+  // var data = new FormData(form);
+  // request.send(data);
+  // request.onreadystatechange = function(){
+  //   if (request.status == 200 && request.readyState == 4) {
+  //     console.log(request.responseText);
+  //   }
+  // }
 }
