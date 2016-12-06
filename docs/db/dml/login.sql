1080ee88f5fd597548c3773d35f5581edec900c2
DamarizLopezS <damarizlopezsoriano@gmail.com>
2016-12-05 21:18:15 -0800

    logins usados para los usuarios

---
+++ docs/db/dml/login.sql
@@ -0,0 +1,23 @@
+create login JaviG
+with password='Sup00a',
+default_database = Restaurant;
+
+create login JonaL
+with password='chef00',
+default_database = Restaurant;
+
+create login GaG
+with password='Sup01b',
+default_database = Restaurant;
+
+create login GilC
+with password='storer00',
+default_database = Restaurant;
+
+create login AntS
+with password='IMaster001a',
+default_database = Restaurant;
+
+create login Administrador
+with password='admin123',
+default_database = Restaurant;
