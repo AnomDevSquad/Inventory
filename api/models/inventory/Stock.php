03b4f2c6043125937560eb5db6c8d255856c76a9
xentyo <xentyo@hotmail.com>
2016-12-06 01:06:22 -0800

    Apis de get stock ya funcionando
    ahora se puede consultar el stock por warehouse

--- api/models/inventory/Stock.php
+++ api/models/inventory/Stock.php
@@ -7,9 +7,7 @@
 
 	class Stock
 	{
-		private $ingredient;
-		private $warehouse;
-		private $quantity;
+		private $ingredient, $warehouse, $quantity, $max, $min;
 
 		public function __construct()
 		{
@@ -28,7 +26,7 @@
 						i.ing_id, i.ing_description,
 						mu.meu_id, mu.meu_description,
 						w.war_id, w.war_name,
-						s.sto_quantity
+						s.sto_quantity, s.sto_max, s.sto_min
 						FROM Inventory.stock s
 						JOIN Kitchen.ingredients i ON s.sto_id_ing = i.ing_id
 						JOIN Inventory.warehouses w ON s.war_id = w.war_id
@@ -51,13 +49,17 @@
 																		odbc_result($data, 'war_name')
 																	);
 						$this->quantity = odbc_result($data, 'sto_quantity');
+						$this->max = odbc_result($data, 'sto_max');
+						$this->min = odbc_result($data, 'sto_min');
 					}
 					$connection->close();
 				}
-				elseif ($argsCount == 3) {
+				elseif ($argsCount == 5) {
 					$this->ingredient = $args[0];
 					$this->warehouse = $args[1];
 					$this->quantity = $args[2];
+					$this->max = $args[3];
+					$this->min = $args[4];
 				}
 			}
 		}
@@ -70,39 +72,93 @@
 
 		public function get_quantity(){return $this->quantity;}
 		public function set_quantity($newVal)	{	$this->quantity = $newVal;}
+		public function get_max(){return $this->max;}
+		public function set_max($newVal)	{	$this->max = $newVal;}
+		public function get_min(){return $this->min;}
+		public function set_min($newVal)	{	$this->min = $newVal;}
 		public function to_json(){
 			return '{
 				"ingredient":'.$this->ingredient->to_json().',
 				"warehouse":'.$this->warehouse->to_json().',
-				"quantity":'.$this->quantity.'
+				"quantity":'.$this->quantity.',
+				"max":'.$this->max.',
+				"min":'.$this->min.'
 			}';
 		}
 
-		public static function get_all_stock(){
-			$list = array();
-			$connection = new SqlServerConnection();
-			// $sql =
-			// '	SELECT i.ing_id, i.ing_description,w.war_id, w.war_name,s.sto_quantity, m.meu_id, m.meu_description
-			// 	FROM Inventory.stock s
-			// 	JOIN Kitchen.ingredients i ON s.sto_id_ing = i.ing_id
-			// 	JOIN Inventory.warehouses w ON s.war_id = w.war_id
-			// 	JOIN Inventory.ingredient_measurements im ON i.ing_id = im.ims_id_ingredient
-			// 	JOIN Inventory.measurementunits m ON im.ims_id_measurement = m.meu_id';
-			$sql = 'SELECT i.ing_id, i.ing_description,w.war_id, w.war_name,s.sto_quantity
-							FROM Inventory.stock s
-							JOIN Kitchen.ingredients i ON s.sto_id_ing = i.ing_id
-							JOIN Inventory.warehouses w ON s.war_id = w.war_id';
+	public static function get_all_stock(){
+		$list = array();
+		$connection = new SqlServerConnection();
+		$sql =
+		'	SELECT i.ing_id, i.ing_description,w.war_id, w.war_name,s.sto_quantity, s.sto_max, s.sto_min, m.meu_id, m.meu_description
+			FROM Inventory.stock s
+			JOIN Kitchen.ingredients i ON s.sto_id_ing = i.ing_id
+			JOIN Inventory.warehouses w ON s.war_id = w.war_id
+			JOIN Inventory.ingredient_measurements im ON i.ing_id = im.ims_id_ingredient
+			JOIN Inventory.measurementunits m ON im.ims_id_measurement = m.meu_id';
+		$data = $connection->execute_query($sql);
+		if(odbc_num_rows($data) < 1) throw new StockEmptyException();
+		while (odbc_fetch_array($data)) {
+			$ingredient = new Ingredient(
+				odbc_result($data, 'ing_id'),
+				odbc_result($data, 'ing_description'),
+				new Measurement(
+					odbc_result($data, 'meu_id'),
+					odbc_result($data, 'meu_description')
+				)
+			);
+			$warehouse = new Warehouse(
+				odbc_result($data, 'war_id'),
+				odbc_result($data, 'war_name')
+			);
+			$quantity = odbc_result($data, 'sto_quantity');
+			$min = odbc_result($data, 'sto_min');
+			$max = odbc_result($data, 'sto_max');
+			array_push($list, new Stock($ingredient, $warehouse, $quantity, $max, $min));
+		}
+		$connection->close();
+		return $list;
+	}
+
+
+	public static function get_warehouse_stock($warehouse_id){
+		$list = array();
+		$connection = new SqlServerConnection();
+		try{
+			$sql =
+			'	SELECT i.ing_id, i.ing_description,w.war_id, w.war_name,s.sto_quantity, s.sto_max, s.sto_min, m.meu_id, m.meu_description
+				FROM Inventory.stock s
+				JOIN Kitchen.ingredients i ON s.sto_id_ing = i.ing_id
+				JOIN Inventory.warehouses w ON s.war_id = w.war_id
+				JOIN Inventory.ingredient_measurements im ON i.ing_id = im.ims_id_ingredient
+				JOIN Inventory.measurementunits m ON im.ims_id_measurement = m.meu_id
+				WHERE w.war_id = %d';
+			$sql = sprintf($sql, $warehouse_id);
 			$data = $connection->execute_query($sql);
 			if(odbc_num_rows($data) < 1) throw new StockEmptyException();
 			while (odbc_fetch_array($data)) {
-				$ingredient = new Ingredient(odbc_result($data, 'ing_id'),odbc_result($data, 'ing_description'),
-			new Measurement(/*odbc_result($data, 'meu_id'),odbc_result($data, 'meu_description')*/));
-				$warehouse = new Warehouse(odbc_result($data, 'war_id'),odbc_result($data, 'war_name'));
+				$ingredient = new Ingredient(
+					odbc_result($data, 'ing_id'),
+					odbc_result($data, 'ing_description'),
+					new Measurement(
+						odbc_result($data, 'meu_id'),
+						odbc_result($data, 'meu_description')
+					)
+				);
+				$warehouse = new Warehouse(
+					odbc_result($data, 'war_id'),
+					odbc_result($data, 'war_name')
+				);
 				$quantity = odbc_result($data, 'sto_quantity');
-				array_push($list, new Stock($ingredient, $warehouse, $quantity));
+				$min = odbc_result($data, 'sto_min');
+				$max = odbc_result($data, 'sto_max');
+				array_push($list, new Stock($ingredient, $warehouse, $quantity, $max, $min));
 			}
-			$connection->close();
-			return $list;
 		}
+		finally{
+			$connection->close();
+		}
+		return $list;
 	}
+}
 ?>
