416ec2c3ac8c121a5ea9fdae952611f49f2296d1
CesarRamos <cesarito052@gmail.com>
2016-12-09 21:28:22 -0800

    borramos la vista de new order para modificarla

--- js/orders.js
+++ js/orders.js
@@ -35,43 +35,16 @@ function initNewOrder() {
     document.getElementById('main_content').innerHTML = '';
     var content = document.querySelector('#main_content');
     var request = new XMLHttpRequest();
-    // var jsonEmployee = JSON.parse(sessionStorage['employee_session']);
-    // request.setRequestHeader('empid', 1);
     request.open('GET', 'api/v1/get_all_dishes.php', true);
     request.send();
     request.onreadystatechange = function() {
         if (request.status == 200 && request.readyState == 4) {
-            loadSections(content);
-            loadDishes(request.responseText);
+            loadDishes(content, request.responseText);
         }
     }
 }
 
-function loadSections(content) {
-    var menu = create(content, 'div', ['id', 'class'], ['menu', '']);
-    for (var i = 0; i < categoryName.length; i++) {
-        create(menu, 'div', ['id', 'class'], ["category_" + idCategory[i], 'category'], '<p>' + categoryName[i] + '</p>');
-    }
-}
-
-function loadDishes(data) {
-    var json = JSON.parse(data);
-    var dishes = json.dishes;
-    for (var i = 0; i < dishes.length; i++) {
-        var item = dishes[i];
-        var section = document.getElementById('category_' + item.category);
-        var button = create(section, 'div', ['id', 'onclick', 'price'], ['dish_' + item.id, 'addDish(this.id)', item.price], '');
-        var divContent = create(button, 'div', ['class'], ['picture']);
-        create(divContent, 'img', ['src'], ['img/dishes/'+item.id + '.jpg']);
-        create(button, 'p', ['class'], ['name'], item.name);
-        create(button, 'p', ['class'], ['price'], ' $ ' + item.price + ' DLLS ');
-    }
-    var content = document.querySelector('#main_content');
-    var order = create(content, 'div', ['id', 'class'], ['orders', '']);
-    create(order, 'p', ['id'], ['title'], 'Order');
-    create(order, 'div', ['id'], ['dishes']);
-    var form = create(order, 'form', ['id', 'method'], ['form', 'post']);
-    create(form, 'input', ['id', 'class', 'onclick', 'type', 'value'], ['submit', 'btn', 'buy();', 'button', 'Buy']);
+function loadDishes(content, data) {
 }
 
 function addDish(id) {
